17c725b216d786d1f17eac0f8e86987a8256aac5
PicanteX <picantex@DESKTOP-OAIDI5H.localdomain>
2023-01-18 21:31:13 -0500

    posts update

--- api/users.js
+++ api/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const usersRouter = express.Router();
 
-usersRouter.use((req, res, next) => {
-  console.log("A request is being made to /users");
+const { getAllUsers } = require('../db');
 
-  res.send({ message: 'hello from /users!' });
+// UPDATE
+usersRouter.get('/', async (req, res) => {
+  const users = await getAllUsers();
+
+  res.send({
+    users
+  });
 });
 
 module.exports = usersRouter;
--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const PORT = 3000;
 const express = require('express');
 const server = express();
 
+const { client } = require('./db');
+client.connect();
+
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT);
 });
 
-<<<<<<< Updated upstream
-=======
 const morgan = require('morgan');
 server.use(morgan('dev'));
 
 server.use(express.json())
+
+server.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
+
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
->>>>>>> Stashed changes

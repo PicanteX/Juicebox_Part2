5ab876c6e5c209e5d89ef41e7b17fed0adc1403e
jessanahi <jess.anahi.zamora@gmail.com>
2023-01-20 18:17:25 -0600

    start of p3

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const postsRouter = express.Router();
-
-const { getAllPosts } = require('../db');
+const { requireUser } = require('./utils');
+const { getAllPosts, createPost } = require('../db');
 
 postsRouter.get('/', async (req, res) => {
   const posts = await getAllPosts();
@@ -9,6 +9,25 @@ postsRouter.get('/', async (req, res) => {
   res.send({
     posts
   });
+});
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = '' } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    const post = await createPost({ title, content, tagArr });
+
+    res.send({ post })
+  } catch ({ name, message }) {
+    next ({ name, message });
+  }
 });
 
 module.exports = postsRouter;
